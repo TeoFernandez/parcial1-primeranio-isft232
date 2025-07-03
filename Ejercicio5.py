52a3467716d39df46cd1c8b26068e32fceac0079
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-07-03 10:59:11 -0300

    Agregar archivo Ejercicio5.py con lógica para calcular y clasificar el IMC

---
+++ Ejercicio5.py
@@ -0,0 +1,17 @@
+# Ingreso de datos
+peso = int(input("Ingresá tu peso en kg (entero): "))
+altura = float(input("Ingresá tu altura en metros (con decimales): "))
+
+# Cálculo del IMC
+imc = peso / (altura * altura)
+
+# Mostrar el resultado del IMC
+print(f"\nTu IMC es: {imc:.2f}")
+
+# Clasificación según IMC
+if imc < 18.5:
+    print("Delgado")
+elif 18.5 <= imc <= 24.9:
+    print("Peso Correcto")
+else:
+    print("Sobrepeso")
