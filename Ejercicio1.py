600dfb24add46ce362dc21bf6d0fe308d5480397
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-07-03 10:30:08 -0300

    Agregar archivo Ejercicio1.py con lógica de evaluación de texto

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+#A este punto lo hiciste bien.
+texto="Hoy es día de evalución de introducción a la programación"
+
+if len(texto)>10:
+    r=texto[7:24].upper()
+else:
+    r=len(texto)
+
+print(r)
+
